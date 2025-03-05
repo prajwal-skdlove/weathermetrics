4f016684e8014e4e8cfd659c25de4c6d0c3eb20e
jhh-ideas <jhh.ideas@gmail.com>
2025-03-05 14:19:02 -0500

    add file to create modeling elements

---
+++ data/weathermetrics/R_Code_Data/R00_MK_01_to_03.R
@@ -0,0 +1,83 @@
+#Run data for a station
+library(data.table)
+
+source("C:/Users/jhugh/Documents/HT/NCEI_data/R01_Expand_Variables_from_Norm_Data.R")
+source("C:/Users/jhugh/Documents/HT/NCEI_data/R02_Create_Modeling_Data.R")
+source("C:/Users/jhugh/Documents/HT/NCEI_data/R03_Create_Modeling_Input_Dataset.R")
+
+fn_create_model_data <- function(in_wd,in_xstation){
+# in_xstation <- vc_stations[48]
+setwd(wd)
+
+########################
+#1. Read normalized data
+#dt_metrics_year_norm <- fread(file=paste(in_xstation,"metrics_norm.cvs",sep="_"))
+##Create metrics file
+#lst_metrics <- fn_aggregate_metrics(wd,in_xstation,dt_metrics_year_norm)
+#fn <- paste(in_xstation,"_lst_metrics",".RDS",sep="")
+#save(lst_metrics,file=fn)
+
+#Get lst_metrics file
+fn <- paste(wd,"\\",in_xstation,"_lst_metrics",".RDS",sep="")
+load(file=fn)
+
+###################################
+#2. Create all the modeling metrics
+precip_model_data <- fn_moddat_precip(lst_metrics[["Precip1"]],
+                                      lst_metrics[["Precip2"]],
+                                      lst_metrics[["Precip3"]])
+
+rel_humidity_model_data <- fn_moddat_rel_humidity(lst_metrics[["RelHum1"]],
+                                                  lst_metrics[["RelHum2"]],
+                                                  lst_metrics[["RelHum3"]])
+
+air_temp_model_data <- fn_moddat_temp(lst_metrics[["Temp"]])
+
+
+lst_mod_data <- list(precip_model_data,
+                     rel_humidity_model_data,
+                     air_temp_model_data)
+names(lst_mod_data) <- c("Precip","RelHum","AirTemp")
+
+fn <- paste(in_xstation,"_lst_mod_dat",".rds",sep="")
+save(lst_mod_data,file=fn)
+
+##################################
+#3. Create csv data sets for outputing
+fn <- paste(in_xstation,"_lst_mod_dat",".RDS",sep="")
+load(file=fn)
+
+#out_name <- paste("model_data_precip",sep="")
+#dt_precip_data_values <- fn_feat_Precip(lst_mod_data[["Precip"]],"PR")
+
+#Write out precip data column
+dt00 <- lst_mod_data[["Precip"]]
+if(nrow(dt00) > 1){
+fwrite(fn_feat_Precip(dt00,"PR"),
+       file=paste(wd,"\\",paste(in_xstation,"_model_data_precip",sep=""),".csv",
+                  sep=""))}
+
+dt00 <- lst_mod_data[["RelHum"]]
+if(nrow(dt00) > 1){
+fwrite(fn_feat_RelHum(dt00,"RH"),
+       file=paste(wd,"\\",paste(in_xstation,"_model_data_relhum",sep=""),".csv",
+                  sep=""))}
+
+dt00 <- lst_mod_data[["AirTemp"]]
+if(nrow(dt00) > 1){
+fwrite(fn_feat_AirTemp(dt00,"AT"),
+       file=paste(wd,"\\",paste(in_xstation,"_model_data_airtemp",sep=""),".csv",
+                  sep=""))}
+
+return(1)}
+
+wd <- "\\\\CXA01\\Users\\jhugh\\Documents\\HT\\NCEI_data"
+setwd(wd)
+dt_stations <- fread(file="Station_Info.csv")
+
+#vc_stations <- dt_stations[US_State %in% c("NY","NJ","TX"),STATION]
+vc_stations <- dt_stations[US_State %in% c("CA"),STATION]
+
+lst_complete <- lapply(vc_stations, function(in_xstation) 
+                    fn_create_model_data(wd,in_xstation))
+
