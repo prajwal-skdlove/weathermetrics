2c5c4edadd3c73c4299282cfa89cdeeea8cfb8a3
jhh-ideas <jhh.ideas@gmail.com>
2025-12-02 15:35:09 -0500

    JH

---
+++ s4model/py_s4b_bin1_5_20_test.bat
@@ -0,0 +1,1 @@
+python -m s4model --modelname s4_70219026615_DAYSUM_bin1_5_20 --modeltype classification --dataset  C:/Users/jhugh/Documents/Py_S4/NCEI_parquet_files/70219026615_DAYSUM_bin1_5_20_train.parquet  --tabulardata --dependent_variable tgt_bin --epochs  100
